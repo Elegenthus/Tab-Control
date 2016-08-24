c9e3f67cc61d453437484de6a35f91b955414def
Elegenthus <996886231@qq.com>
2016-08-24 16:20:29 +0800

    add another solution

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
- var tabs = document.getElementsByClassName('tab'),
+//法一：用立即执行的函数（IIFE）解决变量提升问题
+var tabs = document.getElementsByClassName('tab'),
     box = document.getElementById('table'),
     table = box.getElementsByTagName('div');
-for(let i = 0; i < tabs.length ; i++){
-    tabs[i].addEventListener("click",function(){
-      select(i);
-    });
+for(var i = 0; i < tabs.length ; i++){
+    (function(){
+      var that = i;
+      tabs[that].addEventListener("click",function(){
+        select(that);
+      });
+    })(i);
   }
-    function select(n){
+function select(n){
       for(var i = 0; i < tabs.length ; i++){
          if(i == n){
           tabs[i].className = "tab currenttab";
@@ -17,9 +21,30 @@ for(let i = 0; i < tabs.length ; i++){
           table[i].style.display = "none";
         }
       }     
-    }
-    select(0);
-
-
-
-
+};
+select(0);
+    
+// 
+// 法二：下为将类数组转化成数组的方案
+//    
+//     
+//  var tabs = document.getElementsByClassName('tab'),
+//     box = document.getElementById('table'),
+//     table = box.getElementsByTagName('div');
+//     tabs = Array.prototype.slice.call(tabs);
+//      tabs.forEach(function(tab, i) {
+//       tab.addEventListener("click", function() { select(i); });
+//     });
+//     function select(n){
+//       tabs.forEach(function(tab,i){
+//          if(i == n){
+//           tab.className = "tab currenttab";
+//           table[i].style.display = "table-cell";
+//         }
+//         else{
+//           tab.className = "tab";
+//           table[i].style.display = "none";
+//         }
+//       });     
+//     }
+// select(0);
